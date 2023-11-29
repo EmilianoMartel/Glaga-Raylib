cd096f6d31e82ed4d7088c35159f25eec9bb7d88
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-11-29 13:11:41 -0300

    Implemented BaseSetup

---
+++ Galaga-Raylib/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+
+public class PruebasRayLib
+{
+    public static void Main()
+    {
+        int screenWidth = 1240;
+        int screenHeight = 720;
+        Raylib.InitWindow(screenWidth, screenHeight, "Galaga-Raylib");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
